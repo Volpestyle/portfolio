6341dc2b96d34fa6bd7016c4191f60515f32dda8
James Volpe <volpestyle@gmail.com>
2024-08-18 18:05:44 -0500

    init next project, add google fonts

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Libre_Bodoni } from "@next/font/google";
+import { Hanken_Grotesk } from "@next/font/google";
+
+const libreBodoni = Libre_Bodoni({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  variable: "--font-libre-bodoni",
+});
+
+const hankenGrotesk = Hanken_Grotesk({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  variable: "--font-hanken-grotesk",
+});
+
+export const metadata: Metadata = {
+  title: "JCV's Portfolio",
+  description: "James Volpe's Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={"dark font-sans"}>{children}</body>
+    </html>
+  );
+}
